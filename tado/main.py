013ba98ae1a54c833a4fa7071f17303729f956b2
Aaron Traynor <aaron@primer.io>
2024-09-23 17:03:36 +0100

    Update logger config

--- tado/main.py
+++ tado/main.py
@@ -18,6 +18,12 @@ from tado import (
 
 logger = logging.getLogger(__name__)
 
+logging.basicConfig(
+    format="%(asctime)s [%(levelname)s] - %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
 arg_parser = ArgumentParser()
 
 tado_credentials = get_tado_credentials(arg_parser=arg_parser)
